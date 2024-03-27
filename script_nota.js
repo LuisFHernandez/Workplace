5e53c8615fe891392394d3922e2f54ff0700249c
LuisFHernandez <luisfel.hernandezmar@nube.unadmexico.mx>
2024-03-26 19:23:48 -0600

    Creating automatic note 1
    Creating automatic note

---
+++ script_nota.js
@@ -0,0 +1,20 @@
+const btnCrearNota = document.getElementById("crear-nota");
+const txtFocalizar = document.getElementById("focalizar");
+const txtDeconstruir = document.getElementById("deconstruir");
+const txtAlternativas = document.getElementById("alternativas");
+const txtNota = document.getElementById("nota");
+const btnCopiarNota = document.getElementById("copiar-nota");
+
+btnCrearNota.addEventListener("click", () => {
+  let nota = "";
+  nota += `SE COMUNICA DEBIDO: ${txtFocalizar.value}\n`;
+  nota += `SE ABORDA: ${txtDeconstruir.value}\n`;
+  nota += `SE CANALIZA A: ${txtAlternativas.value}\n`;
+  txtNota.textContent = nota;
+});
+
+btnCopiarNota.addEventListener("click", () => {
+  navigator.clipboard.writeText(txtNota.textContent);
+  alert("¡Texto copiado al portapapeles!");
+});
+

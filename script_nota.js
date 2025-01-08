ee6ba1accc11fbf0f2979005af95730b7fe7994a
LuisFHernandez <luisfel.hernandezmar@nube.unadmexico.mx>
2025-01-08 13:03:08 -0600

    Lower & Uppercase
    Added Button to create lower and uppercase of the text

--- script_nota.js
+++ script_nota.js
@@ -1,3 +1,4 @@
+// Elementos principales del DOM
 const btnCrearNota = document.getElementById("crear-nota");
 const txtFocalizar = document.getElementById("focalizar");
 const txtDeconstruir = document.getElementById("deconstruir");
@@ -5,6 +6,25 @@ const txtAlternativas = document.getElementById("alternativas");
 const txtNota = document.getElementById("nota");
 const btnCopiarNota = document.getElementById("copiar-nota");
 
+// Nuevos botones para mayúsculas y minúsculas
+const btnMinusculas = document.getElementById("minusculas");
+const btnMayusculas = document.getElementById("mayusculas");
+
+// Función: Convertir el texto de las cajas a minúsculas
+btnMinusculas.addEventListener("click", () => {
+  txtFocalizar.value = txtFocalizar.value.toLowerCase();
+  txtDeconstruir.value = txtDeconstruir.value.toLowerCase();
+  txtAlternativas.value = txtAlternativas.value.toLowerCase();
+});
+
+// Función: Convertir el texto de las cajas a mayúsculas
+btnMayusculas.addEventListener("click", () => {
+  txtFocalizar.value = txtFocalizar.value.toUpperCase();
+  txtDeconstruir.value = txtDeconstruir.value.toUpperCase();
+  txtAlternativas.value = txtAlternativas.value.toUpperCase();
+});
+
+// Función: Crear nota con el contenido de las cajas
 btnCrearNota.addEventListener("click", () => {
   let nota = "";
   nota += `SE COMUNICA DEBIDO A ${txtFocalizar.value}\n`;
@@ -13,8 +33,8 @@ btnCrearNota.addEventListener("click", () => {
   txtNota.textContent = nota;
 });
 
+// Función: Copiar el texto de la nota al portapapeles
 btnCopiarNota.addEventListener("click", () => {
   navigator.clipboard.writeText(txtNota.textContent);
   alert("¡Texto copiado al portapapeles!");
 });
-
